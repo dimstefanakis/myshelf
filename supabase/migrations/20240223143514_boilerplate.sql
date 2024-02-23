600ba06095d39152f6b38a6857b9b339f348c8a7
Dimitris <jimstef@outlook.com>
2024-02-23 16:39:43 +0200

    initial migrations

---
+++ supabase/migrations/20240223143514_boilerplate.sql
@@ -0,0 +1,317 @@
+create table "public"."books" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "title" text,
+    "cover_url" text
+);
+
+
+alter table "public"."books" enable row level security;
+
+create table "public"."journals" (
+    "id" uuid not null default gen_random_uuid(),
+    "users_book" uuid,
+    "created_at" timestamp with time zone not null default now(),
+    "title" text,
+    "description" text,
+    "image_url" text
+);
+
+
+alter table "public"."journals" enable row level security;
+
+create table "public"."notes" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "users_book" uuid,
+    "user" uuid,
+    "title" text,
+    "description" text
+);
+
+
+alter table "public"."notes" enable row level security;
+
+create table "public"."users" (
+    "id" uuid not null,
+    "full_name" text,
+    "avatar_url" text,
+    "billing_address" jsonb,
+    "payment_method" jsonb
+);
+
+
+alter table "public"."users" enable row level security;
+
+create table "public"."users_books" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "book" uuid,
+    "status" text
+);
+
+
+alter table "public"."users_books" enable row level security;
+
+CREATE UNIQUE INDEX books_pkey ON public.books USING btree (id);
+
+CREATE UNIQUE INDEX journals_pkey ON public.journals USING btree (id);
+
+CREATE UNIQUE INDEX notes_pkey ON public.notes USING btree (id);
+
+CREATE UNIQUE INDEX users_books_pkey ON public.users_books USING btree (id);
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."books" add constraint "books_pkey" PRIMARY KEY using index "books_pkey";
+
+alter table "public"."journals" add constraint "journals_pkey" PRIMARY KEY using index "journals_pkey";
+
+alter table "public"."notes" add constraint "notes_pkey" PRIMARY KEY using index "notes_pkey";
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."users_books" add constraint "users_books_pkey" PRIMARY KEY using index "users_books_pkey";
+
+alter table "public"."journals" add constraint "journals_users_book_fkey" FOREIGN KEY (users_book) REFERENCES users_books(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."journals" validate constraint "journals_users_book_fkey";
+
+alter table "public"."notes" add constraint "notes_user_fkey" FOREIGN KEY ("user") REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."notes" validate constraint "notes_user_fkey";
+
+alter table "public"."notes" add constraint "notes_users_book_fkey" FOREIGN KEY (users_book) REFERENCES users_books(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."notes" validate constraint "notes_users_book_fkey";
+
+alter table "public"."users_books" add constraint "users_books_book_fkey" FOREIGN KEY (book) REFERENCES books(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."users_books" validate constraint "users_books_book_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public.users (id, full_name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$function$
+;
+
+grant delete on table "public"."books" to "anon";
+
+grant insert on table "public"."books" to "anon";
+
+grant references on table "public"."books" to "anon";
+
+grant select on table "public"."books" to "anon";
+
+grant trigger on table "public"."books" to "anon";
+
+grant truncate on table "public"."books" to "anon";
+
+grant update on table "public"."books" to "anon";
+
+grant delete on table "public"."books" to "authenticated";
+
+grant insert on table "public"."books" to "authenticated";
+
+grant references on table "public"."books" to "authenticated";
+
+grant select on table "public"."books" to "authenticated";
+
+grant trigger on table "public"."books" to "authenticated";
+
+grant truncate on table "public"."books" to "authenticated";
+
+grant update on table "public"."books" to "authenticated";
+
+grant delete on table "public"."books" to "service_role";
+
+grant insert on table "public"."books" to "service_role";
+
+grant references on table "public"."books" to "service_role";
+
+grant select on table "public"."books" to "service_role";
+
+grant trigger on table "public"."books" to "service_role";
+
+grant truncate on table "public"."books" to "service_role";
+
+grant update on table "public"."books" to "service_role";
+
+grant delete on table "public"."journals" to "anon";
+
+grant insert on table "public"."journals" to "anon";
+
+grant references on table "public"."journals" to "anon";
+
+grant select on table "public"."journals" to "anon";
+
+grant trigger on table "public"."journals" to "anon";
+
+grant truncate on table "public"."journals" to "anon";
+
+grant update on table "public"."journals" to "anon";
+
+grant delete on table "public"."journals" to "authenticated";
+
+grant insert on table "public"."journals" to "authenticated";
+
+grant references on table "public"."journals" to "authenticated";
+
+grant select on table "public"."journals" to "authenticated";
+
+grant trigger on table "public"."journals" to "authenticated";
+
+grant truncate on table "public"."journals" to "authenticated";
+
+grant update on table "public"."journals" to "authenticated";
+
+grant delete on table "public"."journals" to "service_role";
+
+grant insert on table "public"."journals" to "service_role";
+
+grant references on table "public"."journals" to "service_role";
+
+grant select on table "public"."journals" to "service_role";
+
+grant trigger on table "public"."journals" to "service_role";
+
+grant truncate on table "public"."journals" to "service_role";
+
+grant update on table "public"."journals" to "service_role";
+
+grant delete on table "public"."notes" to "anon";
+
+grant insert on table "public"."notes" to "anon";
+
+grant references on table "public"."notes" to "anon";
+
+grant select on table "public"."notes" to "anon";
+
+grant trigger on table "public"."notes" to "anon";
+
+grant truncate on table "public"."notes" to "anon";
+
+grant update on table "public"."notes" to "anon";
+
+grant delete on table "public"."notes" to "authenticated";
+
+grant insert on table "public"."notes" to "authenticated";
+
+grant references on table "public"."notes" to "authenticated";
+
+grant select on table "public"."notes" to "authenticated";
+
+grant trigger on table "public"."notes" to "authenticated";
+
+grant truncate on table "public"."notes" to "authenticated";
+
+grant update on table "public"."notes" to "authenticated";
+
+grant delete on table "public"."notes" to "service_role";
+
+grant insert on table "public"."notes" to "service_role";
+
+grant references on table "public"."notes" to "service_role";
+
+grant select on table "public"."notes" to "service_role";
+
+grant trigger on table "public"."notes" to "service_role";
+
+grant truncate on table "public"."notes" to "service_role";
+
+grant update on table "public"."notes" to "service_role";
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant references on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant trigger on table "public"."users" to "anon";
+
+grant truncate on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant references on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant trigger on table "public"."users" to "authenticated";
+
+grant truncate on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant references on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant trigger on table "public"."users" to "service_role";
+
+grant truncate on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+grant delete on table "public"."users_books" to "anon";
+
+grant insert on table "public"."users_books" to "anon";
+
+grant references on table "public"."users_books" to "anon";
+
+grant select on table "public"."users_books" to "anon";
+
+grant trigger on table "public"."users_books" to "anon";
+
+grant truncate on table "public"."users_books" to "anon";
+
+grant update on table "public"."users_books" to "anon";
+
+grant delete on table "public"."users_books" to "authenticated";
+
+grant insert on table "public"."users_books" to "authenticated";
+
+grant references on table "public"."users_books" to "authenticated";
+
+grant select on table "public"."users_books" to "authenticated";
+
+grant trigger on table "public"."users_books" to "authenticated";
+
+grant truncate on table "public"."users_books" to "authenticated";
+
+grant update on table "public"."users_books" to "authenticated";
+
+grant delete on table "public"."users_books" to "service_role";
+
+grant insert on table "public"."users_books" to "service_role";
+
+grant references on table "public"."users_books" to "service_role";
+
+grant select on table "public"."users_books" to "service_role";
+
+grant trigger on table "public"."users_books" to "service_role";
+
+grant truncate on table "public"."users_books" to "service_role";
+
+grant update on table "public"."users_books" to "service_role";
+
+
---
+++ types_db.ts
@@ -0,0 +1,462 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string
+          query?: string
+          variables?: Json
+          extensions?: Json
+        }
+        Returns: Json
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+  public: {
+    Tables: {
+      books: {
+        Row: {
+          cover_url: string | null
+          created_at: string
+          id: string
+          title: string | null
+        }
+        Insert: {
+          cover_url?: string | null
+          created_at?: string
+          id?: string
+          title?: string | null
+        }
+        Update: {
+          cover_url?: string | null
+          created_at?: string
+          id?: string
+          title?: string | null
+        }
+        Relationships: []
+      }
+      journals: {
+        Row: {
+          created_at: string
+          description: string | null
+          id: string
+          image_url: string | null
+          title: string | null
+          users_book: string | null
+        }
+        Insert: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          image_url?: string | null
+          title?: string | null
+          users_book?: string | null
+        }
+        Update: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          image_url?: string | null
+          title?: string | null
+          users_book?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "journals_users_book_fkey"
+            columns: ["users_book"]
+            isOneToOne: false
+            referencedRelation: "users_books"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      notes: {
+        Row: {
+          created_at: string
+          description: string | null
+          id: string
+          title: string | null
+          user: string | null
+          users_book: string | null
+        }
+        Insert: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          title?: string | null
+          user?: string | null
+          users_book?: string | null
+        }
+        Update: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          title?: string | null
+          user?: string | null
+          users_book?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "notes_user_fkey"
+            columns: ["user"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "notes_users_book_fkey"
+            columns: ["users_book"]
+            isOneToOne: false
+            referencedRelation: "users_books"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      users: {
+        Row: {
+          avatar_url: string | null
+          billing_address: Json | null
+          full_name: string | null
+          id: string
+          payment_method: Json | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          billing_address?: Json | null
+          full_name?: string | null
+          id: string
+          payment_method?: Json | null
+        }
+        Update: {
+          avatar_url?: string | null
+          billing_address?: Json | null
+          full_name?: string | null
+          id?: string
+          payment_method?: Json | null
+        }
+        Relationships: []
+      }
+      users_books: {
+        Row: {
+          book: string | null
+          created_at: string
+          id: string
+          status: string | null
+        }
+        Insert: {
+          book?: string | null
+          created_at?: string
+          id?: string
+          status?: string | null
+        }
+        Update: {
+          book?: string | null
+          created_at?: string
+          id?: string
+          status?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "users_books_book_fkey"
+            columns: ["book"]
+            isOneToOne: false
+            referencedRelation: "books"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+  storage: {
+    Tables: {
+      buckets: {
+        Row: {
+          allowed_mime_types: string[] | null
+          avif_autodetection: boolean | null
+          created_at: string | null
+          file_size_limit: number | null
+          id: string
+          name: string
+          owner: string | null
+          owner_id: string | null
+          public: boolean | null
+          updated_at: string | null
+        }
+        Insert: {
+          allowed_mime_types?: string[] | null
+          avif_autodetection?: boolean | null
+          created_at?: string | null
+          file_size_limit?: number | null
+          id: string
+          name: string
+          owner?: string | null
+          owner_id?: string | null
+          public?: boolean | null
+          updated_at?: string | null
+        }
+        Update: {
+          allowed_mime_types?: string[] | null
+          avif_autodetection?: boolean | null
+          created_at?: string | null
+          file_size_limit?: number | null
+          id?: string
+          name?: string
+          owner?: string | null
+          owner_id?: string | null
+          public?: boolean | null
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
+      migrations: {
+        Row: {
+          executed_at: string | null
+          hash: string
+          id: number
+          name: string
+        }
+        Insert: {
+          executed_at?: string | null
+          hash: string
+          id: number
+          name: string
+        }
+        Update: {
+          executed_at?: string | null
+          hash?: string
+          id?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      objects: {
+        Row: {
+          bucket_id: string | null
+          created_at: string | null
+          id: string
+          last_accessed_at: string | null
+          metadata: Json | null
+          name: string | null
+          owner: string | null
+          owner_id: string | null
+          path_tokens: string[] | null
+          updated_at: string | null
+          version: string | null
+        }
+        Insert: {
+          bucket_id?: string | null
+          created_at?: string | null
+          id?: string
+          last_accessed_at?: string | null
+          metadata?: Json | null
+          name?: string | null
+          owner?: string | null
+          owner_id?: string | null
+          path_tokens?: string[] | null
+          updated_at?: string | null
+          version?: string | null
+        }
+        Update: {
+          bucket_id?: string | null
+          created_at?: string | null
+          id?: string
+          last_accessed_at?: string | null
+          metadata?: Json | null
+          name?: string | null
+          owner?: string | null
+          owner_id?: string | null
+          path_tokens?: string[] | null
+          updated_at?: string | null
+          version?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "objects_bucketId_fkey"
+            columns: ["bucket_id"]
+            isOneToOne: false
+            referencedRelation: "buckets"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      can_insert_object: {
+        Args: {
+          bucketid: string
+          name: string
+          owner: string
+          metadata: Json
+        }
+        Returns: undefined
+      }
+      extension: {
+        Args: {
+          name: string
+        }
+        Returns: string
+      }
+      filename: {
+        Args: {
+          name: string
+        }
+        Returns: string
+      }
+      foldername: {
+        Args: {
+          name: string
+        }
+        Returns: unknown
+      }
+      get_size_by_bucket: {
+        Args: Record<PropertyKey, never>
+        Returns: {
+          size: number
+          bucket_id: string
+        }[]
+      }
+      search: {
+        Args: {
+          prefix: string
+          bucketname: string
+          limits?: number
+          levels?: number
+          offsets?: number
+          search?: string
+          sortcolumn?: string
+          sortorder?: string
+        }
+        Returns: {
+          name: string
+          id: string
+          updated_at: string
+          created_at: string
+          last_accessed_at: string
+          metadata: Json
+        }[]
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
+
