8e8810a8c9af57a32e063af14745e9cd7919ced2
Vasilis Kordogiannis <v.kordogiannis@VASILISS-MBP.station>
2024-02-23 22:28:38 +0200

    added logo/title on home page , added navigation bottom of the screen

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+// import EditScreenInfo from '@/components/topNavBar';
+import { Text, View } from '@/components/Themed';
+
+export default function TabTwoScreen() {
+  return (
+    <View >
+      <Text >Search</Text>
+    </View>
+  );
+}
+
+
