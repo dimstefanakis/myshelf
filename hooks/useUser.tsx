1b240562d816792aa4fc0bd830c2d4c645ac9557
Dimitris <jimstef@outlook.com>
2024-02-29 01:41:00 +0200

    basic shelves functionality, still working on it

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -2,24 +2,40 @@ import { useState, useEffect } from "react";
 import { Session } from "@supabase/supabase-js";
 import { supabase } from "../utils/supabase";
 import { useRouter } from "expo-router";
+import type { Database } from "@/types_db";
+
+type UserBook = Database["public"]["Tables"]["users_books"]["Row"] & {
+  book: Database["public"]["Tables"]["books"]["Row"];
+};
+
+type User = {
+  books: UserBook[];
+} & Session["user"];
 
 function useUser() {
   const router = useRouter();
-  const [user, setUser] = useState<any>();
+  const [user, setUser] = useState<User | undefined | null>();
   const [loading, setLoading] = useState<boolean>(true);
   const [session, setSession] = useState<Session | null>(null);
 
   const getUserDetails = (user_id: any) =>
     supabase.from("users").select("*").eq("id", user_id).single();
+  const getUsersBooks = (user_id: any) =>
+    supabase.from("users_books").select("*, book(*)").eq("user", user_id);
 
   async function getUser() {
     const session = await supabase.auth.getSession();
     // const user = await supabase.auth.getUser();
     const userDetail = await getUserDetails(session?.data.session?.user?.id);
+    const usersBooks = await getUsersBooks(session?.data.session?.user?.id);
 
     setUser(
       session?.data.session?.user
-        ? { ...session.data.session.user, ...userDetail.data }
+        ? ({
+            ...session.data.session.user,
+            ...userDetail.data,
+            books: usersBooks.data,
+          } as unknown as User)
         : null,
     );
     setSession(session?.data.session ?? null);
@@ -34,8 +50,17 @@ function useUser() {
     const { data: authListener } = supabase.auth.onAuthStateChange(
       async (_event, session) => {
         const userDetail = await getUserDetails(session?.user.id);
+        const usersBooks = await getUsersBooks(session?.user.id);
 
-        setUser(session?.user ? { ...session.user, ...userDetail.data } : null);
+        setUser(
+          session?.user
+            ? ({
+                ...session.user,
+                ...userDetail.data,
+                books: usersBooks.data,
+              } as unknown as User)
+            : null,
+        );
         setSession(session ?? null);
         setLoading(false);
       },
