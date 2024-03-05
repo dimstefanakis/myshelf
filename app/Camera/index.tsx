6e667381020ebded6cfc751c8db8dc0b4663fc7f
Vasilis Kordogiannis <v.kordogiannis@Vasiliss-MBP.station>
2024-03-05 04:24:34 +0200

    progress on camera

--- app/Camera/index.tsx
+++ app/Camera/index.tsx
@@ -1,45 +1,96 @@
-// CameraComponent.js
 import React, { useEffect, useState } from "react";
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraType } from "expo-camera";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useRef } from "react";
+import { useNavigation } from "expo-router";
 
 const CameraComponent = () => {
   const [type, setType] = useState(CameraType.back);
   const [hasPermission, setHasPermission] = useState(null);
+  const [capturedImage, setCapturedImage] = useState(null);
+    const cameraRef = useRef(null);
+    const navigation = useNavigation();
+
+const handleCapture = async () => {
+  if (cameraRef.current) {
+    const options = { quality: 0.5, base64: true, skipProcessing: true };
+    const data = await cameraRef?.current?.takePictureAsync(options);
+    setCapturedImage(data.uri);
+    navigation.navigate("modalContent/index", { imageUri: data.uri });
+    console.log(data.uri);
+  }
+};
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
   const toggleCameraType = () => {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
   };
 
   if (hasPermission === null) {
-    return <View><Text>Requesting for camera permission...</Text></View>;
+    return (
+      <View>
+        <Text>Requesting for camera permission...</Text>
+      </View>
+    );
   }
   if (hasPermission === false) {
-    return <View><Text>No access to camera</Text></View>;
+    return (
+      <View>
+        <Text>No access to camera</Text>
+      </View>
+    );
   }
 
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type}>
-        <View style={{ flex: 1, backgroundColor: "transparent", flexDirection: "row", justifyContent: "center" }}>
+      <Camera style={{ flex: 1 }} type={type} ref={
+        cameraRef
+      }>
+        <View style={{ flex: 1, backgroundColor: "transparent" }}>
+          {/* Flip Camera Button */}
           <TouchableOpacity
             style={{
-              alignSelf: "flex-end",
-              alignItems: "center",
+              position: "absolute",
+              top: 40, // Adjust top as needed
+              right: 20, // Adjust right as needed
               backgroundColor: "white",
-              margin: 20,
               padding: 10,
               borderRadius: 5,
             }}
-            onPress={toggleCameraType}>
-            <Text style={{ fontSize: 18, color: "black" }}>Flip Camera</Text>
+            onPress={toggleCameraType}
+          >
+              
+              <MaterialIcons
+                name="flip-camera-ios"
+                size={24}
+                color="black"
+              />
+          </TouchableOpacity>
+
+          {/* Capture Button */}
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              bottom: 40, // Adjust bottom as needed
+              alignSelf: "center",
+              backgroundColor: "white",
+              padding: 20,
+              borderRadius: 50, // Makes it circular
+            }}
+            onPress={() => {
+                handleCapture();
+            }}
+          >
+            <Text style={{ fontSize: 18, color: "black" }}>Capture</Text>
           </TouchableOpacity>
         </View>
       </Camera>
--- app/modalContent/index.tsx
+++ app/modalContent/index.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
-import SelectDropdown from 'react-native-select-dropdown';
+import SelectDropdown from "react-native-select-dropdown";
 import { supabase } from "@/utils/supabase";
 import useUser from "@/hooks/useUser";
+import { useNavigation } from "expo-router";
+import { useRoute } from "@react-navigation/native";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+import { decode } from "base64-arraybuffer";
 
 const ModalContentScreen = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
   const [journalData, setJournalData] = useState({
     title: "",
     book: "",
@@ -14,43 +21,64 @@ const ModalContentScreen = () => {
   const user = useUser();
 
   useEffect(() => {
-    if (user?.user?.books?.length?user.user.books.length > 0 && !journalData.book : "") {
-      setJournalData({ ...journalData, book: user?.user?.books[0].id? user.user.books[0].id : "" });
+    if (
+      user?.user?.books?.length
+        ? user.user.books.length > 0 && !journalData.book
+        : ""
+    ) {
+      setJournalData({
+        ...journalData,
+        book: user?.user?.books[0].id ? user.user.books[0].id : "",
+      });
     }
   }, [user]);
 
-  const handleChange = (name:any, value:any) => {
+  useEffect(() => {
+    if (route.params?.imageUri) {
+      setImageUri(route.params.imageUri);
+    }
+  }, [route.params]);
+
+  const handleChange = (name, value) => {
     setJournalData({ ...journalData, [name]: value });
   };
 
-  const handleSubmit = async () => {
-    try {
-      const { data, error } = await supabase
-        .from("journals")
-        .insert([{
-          title: journalData.title,
-          users_book: journalData.book,
-          description: journalData.description,
-        }]);
+  const selectImage = async () => {
+    const options : ImagePicker.ImagePickerOptions = {
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      
+    };
 
-      if (error) throw new Error(error.message);
-      console.log("Submitted data:", data);
-    } catch (error) {
-      console.error("error");
+    const result = await ImagePicker.launchImageLibraryAsync(options);
+
+    if (!result.cancelled) {
+      const img =result.assets[0]
+      const base64 = await FileSystem.readAsStringAsync(img.uri, { encoding: 'base64' });
+      const filePath =`${new Date().getTime()}.${img.type === 'image' ? 'png' : 'mp4'} ` 
+      const contentType = img.type === 'image' ? 'image/png' : 'video/mp4'
+      await supabase.storage.from('images').upload(filePath, decode(base64), { contentType });
+
     }
-  };
+  }
+ 
+    const handleSubmit =() => {
+      selectImage()
+      }
+
+
 
   return (
-    <View style={{ flex: 1, alignItems: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "white" }}>
       <TextInput
         placeholder="Title"
         style={styles.input}
-        onChangeText={(text) => handleChange('title', text)}
+        onChangeText={(text) => handleChange("title", text)}
       />
       <SelectDropdown
-        data={user?.user?.books.map(book => book.book.title) || []}
+        data={user?.user?.books.map((book) => book.book.title) || []}
         onSelect={(selectedItem, index) => {
-          handleChange('book', user?.user?.books[index].id);
+          handleChange("book", user?.user?.books[index].id);
         }}
         buttonTextAfterSelection={(selectedItem, index) => {
           return selectedItem;
@@ -60,15 +88,15 @@ const ModalContentScreen = () => {
         }}
         buttonStyle={styles.dropdown1BtnStyle}
       />
- <TextInput
+      <TextInput
         placeholder="Description"
         multiline={true}
         numberOfLines={4}
         style={styles.multilineInput}
-        onChangeText={(text) => handleChange('description', text)}
+        onChangeText={(text) => handleChange("description", text)}
       />
       <Text style={styles.submitButtonText} onPress={handleSubmit}>
-        Submit
+        Capture & Submit
       </Text>
     </View>
   );
@@ -76,6 +104,7 @@ const ModalContentScreen = () => {
 
 export default ModalContentScreen;
 
+// Styles remain unchanged
 
 const styles = StyleSheet.create({
   input: {
@@ -106,18 +135,17 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   submitButtonText: {
-    color: "white",
+    color: "black",
     fontSize: 16,
     fontWeight: "bold",
   },
   dropdown1BtnStyle: {
-    width: '80%',
+    width: "80%",
     height: 50,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 8,
     borderWidth: 1,
-    borderColor: '#444',
-    marginBottom: 20
-    },
+    borderColor: "#444",
+    marginBottom: 20,
+  },
 });
-
