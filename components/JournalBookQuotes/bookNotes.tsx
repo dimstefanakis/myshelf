eb7df5a8c5a6b43d085c06a57c543c404d75e99c
Vasilis Kordogiannis <v.kordogiannis@Vasiliss-MBP.station>
2024-03-07 13:32:37 +0200

    styles

--- components/JournalBookQuotes/bookNotes.tsx
+++ components/JournalBookQuotes/bookNotes.tsx
@@ -1,76 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Text,
   View,
-  Button,
   TouchableOpacity,
   StyleSheet,
   ScrollView,
   Image,
 } from "react-native";
 import useUser from "@/hooks/useUser";
-import { useState } from "react";
 import { supabase } from "@/utils/supabase";
-// import { useRouter } from 'expo-router';
 import { useNavigation } from "@react-navigation/native";
 
 const BookScreen = () => {
-  const [bookData, setBookData] = useState([
-    {
-      title: "",
-      description: "",
-      users_book: "",
-      user: "",
-    },
-  ]);
+  const [bookData, setBookData] = useState([]);
 
   const navigation = useNavigation();
+  const user = useUser();
 
-  const getThumbnailUrl = (item: any) => {
+  const getThumbnailUrl = (item:any) => {
     return item.users_books?.book?.cover_url ?? "default_thumbnail_url";
   };
 
-  const user = useUser();
-
   const getBookData = async () => {
     let { data, error } = await supabase.from("notes").select(`
-    id,
-    title,
-    description,
-    created_at,
-    user,
-    users_books (*, 
-      book (
-        *,
-        google_api_data
+      id,
+      title,
+      description,
+      created_at,
+      user,
+      users_books (*, 
+        book (
+          *,
+          google_api_data
+        )
       )
-    )
-  `);
+    `);
 
     if (error) {
       console.error("Error fetching data:", error);
       return;
     }
-    setBookData([]);
-    data?.forEach((item) => {
-      setBookData((prevData) => [...prevData, item]);
-    });
+    setBookData(data ?? []);
   };
 
   useEffect(() => {
     getBookData();
   }, []);
+
   return (
-    <View
-      style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {/* button to open modal */}
+    <View style={styles.container}>
       <TouchableOpacity
         style={styles.createBookButton}
         onPress={() => navigation.navigate("ModalBookScreen")}
@@ -80,22 +58,27 @@ const BookScreen = () => {
       {bookData.length > 0 ? (
         <ScrollView
           style={styles.scrollView}
-          contentContainerStyle={{ paddingBottom: 46 }}
+          contentContainerStyle={styles.booksContainer}
           scrollEventThrottle={20}
         >
-          {bookData.map((item: any, index) => {
+          {bookData.map((item:any, index) => {
             const thumbnailUrl = getThumbnailUrl(item);
-            item;
+            // Calculate dynamic styling for alignment
+            const remainder = (index + 1) % 3; // Determine position in the row
+            let additionalStyle = {};
+            if (remainder === 1) { // First item in a row
+              additionalStyle = { marginRight: 'auto', marginLeft: 0 };
+            } else if (remainder === 0) { // Last item in a row
+              additionalStyle = { marginLeft: 'auto', marginRight: 0 };
+            } // Middle item naturally centers due to justifyContent
             return (
-              <View key={item.id} style={{ padding: 20 }}>
+              <View key={item?.id} style={[styles.bookItem, additionalStyle]}>
                 <Image
                   source={{ uri: thumbnailUrl }}
-                  style={{ width: 100, height: 100 }}
+                  style={styles.bookImage}
                 />
-                <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-                  {item.title}
-                </Text>
-                <Text>{item.description}</Text>
+                <Text style={styles.bookTitle}>{item?.title}</Text>
+                <Text style={styles.bookDescription}>{item?.description}</Text>
               </View>
             );
           })}
@@ -110,6 +93,11 @@ const BookScreen = () => {
 export default BookScreen;
 
 const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+  },
   createBookButton: {
     backgroundColor: "#ff9999",
     padding: 10,
@@ -122,6 +110,31 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     width: "100%",
+  },
+  booksContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between", // Adjusted for spacing
     paddingBottom: 50,
+  },
+  bookItem: {
+    width: "30%", // Adjusted for margin inclusion
+    padding: 10,
+    alignItems: "center",
+    marginBottom: 20,
+    // Add margin for spacing
+    marginHorizontal: "1.5%", // Adjust based on desired spacing
+  },
+  bookImage: {
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  bookTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  bookDescription: {
+    textAlign: "center",
   },
 });
