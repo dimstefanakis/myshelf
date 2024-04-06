8fb34c81d7cec493c0279fef7b58886042e1c49b
Dimitris <jimstef@outlook.com>
2024-04-06 11:58:34 +0300

    dev build, google places autocomplete

--- app/gridNavigation/map.tsx
+++ app/gridNavigation/map.tsx
@@ -1,13 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import MapViewScreen from '@/components/MapView'
+import { View, Text } from "react-native";
+import React from "react";
+import MapViewScreen from "@/components/MapView";
 
 const MapScreen = () => {
   return (
-    <View style={{height:"100%"}}>
-        <MapViewScreen />
+    <View style={{ height: "100%" }}>
+      <MapViewScreen />
     </View>
-  )
-}
+  );
+};
 
-export default MapScreen
+export default MapScreen;
